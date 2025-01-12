a74e971599e63a79775a2bafcc3e236b032f33aa
Owaz <owaz.notsdg@gmail.com>
2025-01-12 22:33:34 +0300

    CL::A 0.0.2D - added scene holder class and improved button

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # DEMO WIP Build of game with main menu and some simple classes only
 import pygame
+from random import randint, randrange, random
 
 
 class RenderableImage:  # base image container made to be configurable
     # базовый контейнер изображения для рендера сделанный для удобной конфигурации
-    def __init__(self, filepath, x, y, isalpha=False, do_render=True):
+    def __init__(self, filepath, x, y, tw=0, th=0, isalpha=False, do_render=True):
         if isalpha:
             self.img = pygame.image.load(filepath).convert_alpha()
         else:
             self.img = pygame.image.load(filepath).convert()
+        if not (tw <= 0 or th <= 0):
+            self.img = pygame.transform.scale(self.img, (tw, th))
         self.scenepos = (x, y)  # local scene position for img
         self.do_render = do_render
 
@@ -25,9 +28,34 @@ class RenderableImage:  # base image container made to be configurable
 class ParallaxImage(RenderableImage):  # container for image with all parallax (offset with the cursor) data about it
     # базовый контейнер рендер-изображения, дополненный данными для параллакса
     # справка::параллакс - эффект смещения, в данном контексте относительно курсора
-    def __init__(self, filepath, x, y, pm=1.0, isalpha=False, do_render=True):
-        super().__init__(filepath, x, y, isalpha, do_render)
+    def __init__(self, filepath, x, y, tw=0, th=0, pm=1.0, isalpha=False, do_render=True):
+        super().__init__(filepath, x, y, tw, th, isalpha, do_render)
         self.para_mul = pm  # parallax offset multiplier/множитель параллакс-смещения
+
+
+class SnowflakeSprite(pygame.sprite.Sprite):
+    sflake_0 = pygame.image.load(r"gamedata\img\mmenu\CLA_SDrop__0001s_0003_Snowdrop_0.png")
+    sflake_1 = pygame.image.load(r"gamedata\img\mmenu\CLA_SDrop__0001s_0002_Snowdrop_1.png")
+    sflake_2 = pygame.image.load(r"gamedata\img\mmenu\CLA_SDrop__0001s_0001_Snowdrop_2.png")
+    sflake_3 = pygame.image.load(r"gamedata\img\mmenu\CLA_Sdrop_3.png")
+    sflake_list = [sflake_0, sflake_1, sflake_2, sflake_3]
+
+    def __init__(self, *sgroup):
+        super().__init__(*sgroup)
+        simg = SnowflakeSprite.sflake_list[randint(0, 4) - 1]
+        scale = (random() + 0.5) / 1.5
+        simg = pygame.transform.scale(simg, (simg.get_width() * scale, simg.get_height() * scale))
+        self.image = pygame.transform.rotate(simg, randrange(-181, 180))
+        self.rect = self.image.get_rect()
+        self.rect.x = randrange(SCREENRES.current_w)
+        self.rect.y = randrange(SCREENRES.current_h)
+
+    def update(self):
+        if self.rect.y >= SCREENRES.current_h:
+            self.rect.y = -50
+        if self.rect.x < 0:
+            self.rect.x = SCREENRES.current_w
+        self.rect = self.rect.move((randrange(12) - 12, 5 + randrange(5)))
 
 
 class PushBtn:  # PUSHBuTtoN class: pushable/класс нажимаемой кнопки
@@ -102,8 +130,11 @@ class PushBtn:  # PUSHBuTtoN class: pushable/класс нажимаемой к�
 class BaseScene:  # scene class base: just a holder for scene content
     # базовый класс сцены - контейнера для содержимого на экране (хотя иногда и за его пределами) в данный м.вр.
     def __init__(self):
+        self.para_l = (0, 0)
         self.imghld = []
         self.btnhld = []
+        self.sgroup = pygame.sprite.Group()
+        self.entgroup = pygame.sprite.Group()
 
     def append_img(self, img):
         self.imghld.append(img)
@@ -117,22 +148,43 @@ class BaseScene:  # scene class base: just a holder for scene content
     def extend_btn(self, btns):
         self.btnhld.extend(btns)
 
+    def append_s(self, spr):
+        spr(self.sgroup)
+
+    def extend_s(self, sprs):
+        for _ in sprs:
+            _(self.sgroup)
+
+    def append_ent(self, ent):
+        ent(self.sgroup)
+
+    def extend_ents(self, ents):
+        for _ in ents:
+            _(self.sgroup)
+
     def btn_validator(self, event, click=False):
         for _ in self.btnhld:
             _.status_check(event, click)
 
-
-class ParallaxScene(BaseScene):  # parallax-supported scene/сцена с поддержкой параллакса
-    def __init__(self):
-        super().__init__()
-
-    def render(self, screen, xoffset, yoffset):
+    def render(self, screen):
         for _ in self.imghld:
             if _.do_render:
-                screen.blit(_.img, (_.scenepos[0] - (xoffset * _.para_mul), _.scenepos[1] - (yoffset * _.para_mul)))
+                if type(_) == ParallaxImage:
+                    screen.blit(_.img, (
+                    _.scenepos[0] - (self.para_l[0] * _.para_mul), _.scenepos[1] - (self.para_l[1] * _.para_mul)))
+                else:
+                    screen.blit(_.img, (_.scenepos[0], _.scenepos[1]))
         for _ in self.btnhld:
             if _.do_render:
-                _.render(screen, (xoffset, yoffset))
+                _.render(screen, (self.para_l[0], self.para_l[1]))
+        self.sgroup.draw(screen)
+        self.sgroup.update()
+
+    def get_para(self, x, y):
+        self.para_l = (x, y)
+
+    def sprite_event(self, event):
+        self.sgroup.update(event)
 
 
 class SceneHolder:
@@ -141,8 +193,12 @@ class SceneHolder:
         self.fade_timer = 0
         self.no_event = False
 
-    def render(self, screen, *params):
-        self.scene.render(screen, *params)
+    def render(self, screen):
+        if not self.no_event:
+            screen.fill('#000000')
+        else:
+            self.render_fade(screen)
+        self.scene.render(screen)
 
     def render_fade(self, screen):
         if self.fade_timer <= 255:
@@ -176,25 +232,20 @@ def cla_mainmenu_draw(screen, xoffset=0, yoffset=0):  # REMOVE LATER
 
 
 def mmenu_imgs_init():
-    mmenuimg = ParallaxImage(r'gamedata\img\mmenu\CLA_MM_BG_0.png', 0, 0)
-    mmenuimg.img = pygame.transform.scale(mmenuimg.img, (SCREENRES.current_w, SCREENRES.current_w / 2 * 1.25))
+    mmenuimg = ParallaxImage(r'gamedata\img\mmenu\CLA_MM_BG_0.png', 0, 0, SCREENRES.current_w + 40,
+                             int(SCREENRES.current_w / 2 * 1.25) + 50)
     clachr = ParallaxImage(r'gamedata\img\mmenu\CLA_Chr__0003s_0004_HD_Obvod.png', SCREENRES.current_w / 2 - 512,
-                           SCREENRES.current_h - 974, 1.5, True)
-    clachr.img = pygame.transform.scale(clachr.img, (1024, 1024))
+                           SCREENRES.current_h - 974, 1024, 1024, 1.5, True)
     clachr_g = ParallaxImage(r'gamedata\img\mmenu\CLA_Chr__0003s_0001_HD_GS_Zakras.png',
-                             SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1.5, True, False)
-    clachr_g.img = pygame.transform.scale(clachr_g.img, (1024, 1024))
+                             SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1024, 1024, 1.5, True, False)
     clachr_bld = ParallaxImage(r'gamedata\img\mmenu\CLA_Chr__0003s_0002_BLD.png',
-                               SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1.5, True)
-    clachr_bld.img = pygame.transform.scale(clachr_bld.img, (1024, 1024))
+                               SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1024, 1024, 1.5, True)
     clachr_col = ParallaxImage(r'gamedata\img\mmenu\CLA_Chr__0003s_0003_HD_ColOL.png',
-                               SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1.5, True)
-    clachr_col.img = pygame.transform.scale(clachr_col.img, (1024, 1024))
+                               SCREENRES.current_w / 2 - 512, SCREENRES.current_h - 974, 1024, 1024, 1.5, True)
     clachr_gno = ParallaxImage(r'gamedata\img\mmenu\CLA_Chr__0003s_0000_gno.png',
-                               SCREENRES.current_w / 2 - 512 - 256 - 128, SCREENRES.current_h - 512 - 256, 4, True)
-    clachr_gno.img = pygame.transform.scale(clachr_gno.img, (1024, 1024))
-    ttle = ParallaxImage(r'gamedata\img\mmenu\CLA_Txt_0.png', SCREENRES.current_w / 2 - 1024, 0, 2.5, True)
-    ttle.img = pygame.transform.scale(ttle.img, (2048, 512))
+                               SCREENRES.current_w / 2 - 512 - 256 - 128, SCREENRES.current_h - 512 - 256, 1024, 1024,
+                               4, True)
+    ttle = ParallaxImage(r'gamedata\img\mmenu\CLA_Txt_0.png', SCREENRES.current_w / 2 - 1024, 0, 2048, 512, 2.5, True)
     return mmenuimg, clachr, clachr_col, clachr_bld, ttle, clachr_gno, clachr_g
 
 
@@ -213,13 +264,14 @@ def doomkisser_enabler():  # FOR BUTTONS TEST
     global do_render_pasxalko
     do_render_pasxalko = not do_render_pasxalko
     ui_click.play()
+    pygame.mixer.music.fadeout(5000)
     sceneslot.switch_scene(nmenu)
 
 
 pygame.init()
 pygame.mixer.init()  # test
 mtimer = pygame.time  # costill
-pygame.mixer.music.load(r"gamedata\aud\mus\svdetelstvo_polko_privet.mp3")  # test (change it later like move to separate
+pygame.mixer.music.load(r"gamedata\aud\mus\dymyat_molcha.mp3")  # test (change it later like move to separate
 # def or class idk)
 pygame.mixer.music.play()  # test (look 124)
 pygame.mixer.music.set_volume(1.0)  # test (look 124)
@@ -234,7 +286,7 @@ game_isactive = True
 do_render_pasxalko = False
 font_0 = pygame.font.Font(None, 35)  # font
 font_0.set_bold(True)
-mmenu = ParallaxScene()  # wip (subject to be properly organized probably)
+mmenu = BaseScene()  # wip (subject to be properly organized probably)
 mmenu.extend_img(mmenu_imgs_init())  # wip (subject to be properly organized probably)
 # pygame.mixer.music.fadeout(10000)
 testbtn = PushBtn(x=SCREENRES.current_w // 2 - 100, y=SCREENRES.current_h // 2 + 256, btntxt="kill program")
@@ -245,31 +297,24 @@ testbtn2 = PushBtn(w=450, x=SCREENRES.current_w // 2 - 225, y=SCREENRES.current_
                    btntxt="Show a cute boykisser doomer UwU")  # AS WELL AS 161
 testbtn2.set_func(doomkisser_enabler)  # SAME AS 162
 mmenu.append_btn(testbtn2)
-nmenu = ParallaxScene()  # wip (subject to be properly organized probably)
+for _ in range(10):
+    mmenu.append_ent(SnowflakeSprite)
+nmenu = BaseScene()  # wip (subject to be properly organized probably)
 nmenu.append_btn(testbtn2)
 sceneslot = SceneHolder(mmenu)
 dtime = 0  # costill
 while game_isactive:
     if (mtimer.get_ticks() + dtime) % 170000 >= 169995:  # nowayroyatnee costill (subject to be removed)
         dtime += music_controller()
-    if sceneslot.no_event:
-        sceneslot.render_fade(screen)
-        sceneslot.render(screen, 0, 0)
-        pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_isactive = False
         if event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONUP:  # costill a little bit
-            # mmenu.btn_validator(event)
-            # mmenu.render(screen, xoffset=event.pos[0] / 50, yoffset=event.pos[1] / 50)
             sceneslot.funccall('btn_validator', event)
-            sceneslot.render(screen, event.pos[0] / 50, event.pos[1] / 50)
-            if do_render_pasxalko:
-                cla_mainmenu_draw(screen, xoffset=event.pos[0] / 50, yoffset=event.pos[1] / 50)
-            pygame.display.flip()
+            sceneslot.funccall('get_para', event.pos[0] / 50, event.pos[1] / 50)
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # mmenu.btn_validator(event, True)
-            # mmenu.render(screen, xoffset=event.pos[0] / 50, yoffset=event.pos[1] / 50)
             sceneslot.funccall('btn_validator', event, True)
-            sceneslot.render(screen, event.pos[0] / 50, event.pos[1] / 50)
-            pygame.display.flip()
+    sceneslot.render(screen)
+    if do_render_pasxalko:
+        cla_mainmenu_draw(screen, xoffset=0, yoffset=0)
+    pygame.display.flip()
